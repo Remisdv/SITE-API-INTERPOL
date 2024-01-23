68795fd00830b1ac897910cb96ce08a613770352
jejman <ant.qlc@gmail.com>
2024-01-23 11:15:21 +0100

    add api get info 20 by 20

--- js/test.js
+++ js/test.js
@@ -1,16 +1,49 @@
+let start = 1;
+
 const getRedNotices = async () => {
-    const rep = await fetch('https://ws-public.interpol.int/notices/v1/red?resultPerPage=100');
+    let url = 'https://ws-public.interpol.int/notices/v1/red?resultPerPage=' + (20 * start) + '&page=' + start;
+    const rep = await fetch(url);
     const data = await rep.json();
     return data
 }
 
 function getAllInfo(data) {
-    let str = data._embedded.notices[0].name + " " + data._embedded.notices[0].forename;
-    document.write(str);
+    let list = [{ name: 'name', forname: 'value' }];
+    for (let i = 0; i < 20; i++) {
+        let obj = { name: data.results[i].name, forname: data.results[i].forename };
+        list.push(obj);
+    }
+    console.log(list);
+    // disp info
 }
 
 getRedNotices().then((data) => {
-    //let jej = JSON.stringify(data);
+    let jej = JSON.stringify(data);
     console.log(data);
     getAllInfo(data);
 })
+
+function change_page() {
+    const next = document.getElementById('buton_next');
+    const back = document.getElementById('buton_back');
+
+    next.addEventListener('click', () => {
+        start++;
+        getRedNotices().then((data) => {
+            let jej = JSON.stringify(data);
+            console.log(data);
+            getAllInfo(data);
+        })
+    })
+
+    back.addEventListener('click', () => {
+        if (start > 1) {
+            start--;
+            getRedNotices().then((data) => {
+                let jej = JSON.stringify(data);
+                console.log(data);
+                getAllInfo(data);
+            })
+        }
+    })
+}   
