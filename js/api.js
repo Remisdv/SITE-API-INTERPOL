8490a7dd907b984d80e03f93e8629681b909b1c4
Remi <remi.lhuillier@supdevinci-edu.fr>
2024-01-30 15:32:05 +0100

    GROSSE update
    Tout marche plus que a commenter et a finir de mettre en forme

--- js/api.js
+++ js/api.js
@@ -1,10 +1,12 @@
 let start = 1;
+let redNoticesData; // Variable pour stocker les données récupérées de l'API
 
 const getRedNotices = async () => {
     let url = 'https://ws-public.interpol.int/notices/v1/red?resultPerPage=20&page=' + start;
     console.log(start);
     const rep = await fetch(url);
     const data = await rep.json();
+    redNoticesData = data; // Stocker les données dans la variable globale
     return data;
 }
 
@@ -12,18 +14,73 @@ function getAllInfo(data) {
     let list = data._embedded.notices;
     console.log(list);
     console.log(data);
-        const noticeListDiv = document.getElementById('noticeList');
+
+    const noticeListDiv = document.getElementById('noticeList');
     const ul = document.createElement('ul');
+
     list.forEach(item => {
         const li = document.createElement('li');
-        li.textContent = `${item.name} ${item.forename}`;
+        li.textContent = `NOM : ${item.name}`;
         li.setAttribute('id', item.entity_id);
+
+        // Ajouter un gestionnaire d'événements au clic de l'élément li
+        li.addEventListener('click', () => {
+            // Récupérer les informations stockées au lieu d'appeler l'API à nouveau
+            showStoredInfo(item.entity_id);
+        });
+
         ul.appendChild(li);
     });
+
     noticeListDiv.innerHTML = '';
     noticeListDiv.appendChild(ul);
-
 }
+
+function showStoredInfo(entityId) {
+    // Utiliser les données stockées pour récupérer les informations complémentaires
+    const selectedNotice = redNoticesData._embedded.notices.find(item => item.entity_id === entityId);
+    if (selectedNotice) {
+        // Afficher les informations complémentaires sur la page
+        displayInfoOnPage(selectedNotice);
+    } else {
+        console.error('Les informations complémentaires pour l\'ID spécifié n\'ont pas été trouvées.');
+    }
+}
+
+
+
+function displayInfoOnPage(notice) {
+    const infoContainer = document.getElementById('infoContainer');
+
+    // Créer des éléments pour chaque information et les ajouter au conteneur
+    const nameElement = document.createElement('p');
+    nameElement.textContent = `Nom: ${notice.name}`;
+
+    const forenameElement = document.createElement('p');
+    forenameElement.textContent = `Prénom: ${notice.forename}`;
+
+    const dobElement = document.createElement('p');
+    dobElement.textContent = `Date de naissance: ${notice.date_of_birth}`;
+
+    // Ajouter une image
+    const thumbnailElement = document.createElement('img');
+    thumbnailElement.src = notice._links.thumbnail.href;
+    thumbnailElement.alt = "Image de la notice";
+
+    // Effacer le contenu précédent du conteneur
+    infoContainer.innerHTML = '';
+
+    // Ajouter les nouveaux éléments au conteneur
+    infoContainer.appendChild(nameElement);
+    infoContainer.appendChild(forenameElement);
+    infoContainer.appendChild(dobElement);
+    infoContainer.appendChild(thumbnailElement);
+
+    // Ajouter d'autres éléments selon vos besoins
+}
+
+
+
 
 function change_page() {
     const next = document.getElementById('button_next');
@@ -46,10 +103,6 @@ function change_page() {
         }
     })
 }
-
-function show_more() {
-
-};
 
 getRedNotices().then((data) => {
     getAllInfo(data);
--- js/recherche.js
+++ js/recherche.js
@@ -1,24 +1,24 @@
 async function search() {
-  // Récupérer la valeur de la barre de recherche
   var searchTerm = document.getElementById("search-input").value;
-
   try {
-    // Get the list of names
     const data = await getRedNotices();
     var list = data._embedded.notices;
 
     // Check if searchTerm is in the list of names
-    if (est_dans_la_liste(searchTerm, list)) {
-      // Afficher le résultat
-      var resultContainer = document.getElementById("search-result");
-      resultContainer.innerHTML = searchTerm + ' a une Red Notice.';
-
-    } else {
-      var resultContainer = document.getElementById("search-result");
-      resultContainer.innerHTML = searchTerm + " n'a pas de Red Notice";
-    }
+    const searchResult = est_dans_la_liste(searchTerm, list);
+    updateSearchResult(searchResult, searchTerm);
   } catch (error) {
     console.error('Une erreur s\'est produite lors de la recherche :', error);
+  }
+}
+
+function updateSearchResult(result, searchTerm) {
+  var resultContainer = document.getElementById("search-result");
+  if (result.found) {
+    resultContainer.innerHTML = `${searchTerm} a une Red Notice.`;
+    getall(result.personneTrouveeListe);
+  } else {
+    resultContainer.innerHTML = `${searchTerm} n'a pas de Red Notice`;
   }
 }
 
@@ -29,6 +29,7 @@ const getRedNotices = async () => {
   console.log(start);
   const rep = await fetch(url);
   const data = await rep.json();
+  console.log(data);
   return data;
 }
 
@@ -37,7 +38,7 @@ function est_dans_la_liste(nom, liste) {
   // Check if the list is defined and is an array
   if (Array.isArray(liste)) {
     for (let i = 0; i < liste.length; i++) {
-        if (liste[i].forename === nom) {
+        if (liste[i].name == nom) {
             // Créer une nouvelle liste pour stocker les informations de la personne recherchée
             let personneTrouveeListe = [];
 
@@ -62,11 +63,44 @@ function getall(list) {
     list.forEach(item => {
         const id = item.entity_id;
         console.log("id: "+id);
-        getImage(id)
+        getImage(id);
+        //affiche_img();
+        //affiche chaque élément les un en dessous des autres
+        //const li = document.createElement('li');
+        //li.textContent = `${item.name} ${item.forename} ${item.date_of_birth} ${item.nationalities} ${item.entity_id}`;
+        //li.setAttribute('id', item.entity_id);
+        //ul.appendChild(li);
+        //affiche le nom ${item.name}
         const li = document.createElement('li');
-        li.textContent = `${item.name} ${item.forename} ${item.date_of_birth} ${item.nationalities} ${item.entity_id}`;
+        li.textContent = `Nom : ${item.name} `;
         li.setAttribute('id', item.entity_id);
         ul.appendChild(li);
+        //affiche le prenom ${item.forename}
+        const li1 = document.createElement('li');
+        li1.textContent = `Prénom : ${item.forename}`;
+        li1.setAttribute('id', item.entity_id);
+        ul.appendChild(li1);
+        //affiche la date de naissance ${item.date_of_birth}
+        const li2 = document.createElement('li');
+        li2.textContent = `Date de naissance : ${item.date_of_birth}`;
+        li2.setAttribute('id', item.entity_id);
+        ul.appendChild(li2);
+        //affiche la nationalité ${item.nationalities}
+        const li3 = document.createElement('li');
+        li3.textContent = `Nationalité : ${item.nationalities}`;
+        li3.setAttribute('id', item.entity_id);
+        ul.appendChild(li3);
+        //affiche l'id ${item.entity_id}
+        const li4 = document.createElement('li');
+        li4.textContent = `ID : ${item.entity_id}`;
+        li4.setAttribute('id', item.entity_id);
+        ul.appendChild(li4);
+
+        //affiche l'image ${item._links.thumbnail.href}
+        const img = document.createElement('img');
+        img.setAttribute('src', item._links.thumbnail.href);
+        ul.appendChild(img);
+
         
     });
 
@@ -76,9 +110,11 @@ function getall(list) {
 }
 
 async function getImage(id) {
-    const url = 'https://ws-public.interpol.int/notices/v1/red/2022%2F82960/images';
+    const url = 'https://ws-public.interpol.int/notices/v1/red/2023-86196/images';
     const rep = await fetch(url);
     const data_img = await rep.json();
     console.log(data_img);
     return data_img;
 }
+
+
